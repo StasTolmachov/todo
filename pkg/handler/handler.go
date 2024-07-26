d73f99b69153f43d4daa251e1cdfec0d151dc684
StasTolmachov <stas@tolmachov.dev>
2024-07-26 07:30:23 +0200

    video 2

---
+++ pkg/handler/handler.go
@@ -0,0 +1,38 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+type Handler struct {
+}
+
+func (h *Handler) InitRoutes() *gin.Engine {
+	router := gin.New()
+
+	auth := router.Group("/auth")
+	{
+		auth.POST("/sing-up", h.singUp)
+		auth.POST("/sing-in", h.singIn)
+	}
+
+	api := router.Group("/api")
+	{
+		lists := api.Group("/lists")
+		{
+			lists.POST("/", h.creatList)
+			lists.GET("/", h.getAllList)
+			lists.GET("/:id", h.getListById)
+			lists.PUT("/:id", h.updateList)
+			lists.DELETE("/:id", h.deletList)
+
+			items := lists.Group(":id/items")
+			{
+				items.POST("/", h.creatItem)
+				items.GET("/", h.getAllItem)
+				items.GET("/:item_id", h.getItemById)
+				items.DELETE("/:item_id", h.deletItem)
+			}
+
+		}
+	}
+	return router
+}
